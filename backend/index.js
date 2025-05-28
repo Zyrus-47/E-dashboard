22adf5d7952a78eb61ae010c9379baf720f46610
Zyrus-47 <140943539+Zyrus-47@users.noreply.github.com>
2025-05-28 23:14:29 +0530

    make route for api and remove password from regisrter and login api

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,27 @@ app.use(cors());
 app.post("/register",async(req,resp)=>{
     let user=new User(req.body);
     let result=await user.save();
+    result=result.toObject();
+    delete result.password
     resp.send(result);
 })
+
+app.post("/login", async (req, resp) => {
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne({
+            email: req.body.email,
+            password: req.body.password
+        }).select("-password");
+
+        if (user) {
+            resp.send(user);
+        } else {
+            resp.send({ result: "No user found" });
+        }
+    } else {
+        resp.send({ result: "Email and password required" });
+    }
+});
+
 
 app.listen(5000);
