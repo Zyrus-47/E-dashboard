65b95e8708a21dca7b34d6837af1501fe914dbe3
Zyrus-47 <140943539+Zyrus-47@users.noreply.github.com>
2025-05-29 10:22:12 +0530

    hide product menu before login

--- front end/Edashboard/src/components/Nav.jsx
+++ front end/Edashboard/src/components/Nav.jsx
@@ -12,21 +12,20 @@ const Nav = () => {
 
   return (
     <div>
-      <ul className="nav-ul">
+     {auth? <ul className="nav-ul">
         <li><Link to="/">Product</Link></li>
         <li><Link to="/add">Add Product</Link></li>
         <li><Link to="/update">Update Product</Link></li>
         <li><Link to="/profile">Profile</Link></li>
-        {
-          auth ?<li> <Link onClick={logout} to="/signup">Logout</Link> </li>
-          : 
-          <>
-           <li><Link to="/signup">Sign Up</Link></li>
-           <li><Link to="/login">Login</Link></li>
-          </>
-
-        }
+        <li> <Link onClick={logout} to="/signup">Logout</Link> </li>
+       
       </ul>
+      :
+      <ul className="nav-ul nav-right">
+        <li><Link to="/signup">Sign Up</Link></li>
+           <li><Link to="/login">Login</Link></li>
+      </ul>
+}
     </div>
   );
 };
