2a3bc488bbe443d679252f22061a0d02490a8bf7
Zyrus-47 <140943539+Zyrus-47@users.noreply.github.com>
2025-05-29 10:00:20 +0530

    set local storage and reirected login logout pages

--- front end/Edashboard/src/components/Login.jsx
+++ front end/Edashboard/src/components/Login.jsx
@@ -1,11 +1,35 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
+    const navigate=useNavigate();
+      useEffect(()=>{
+          const auth=localStorage.getItem("user");
+          if(auth)
+          {
+            navigate('/')
+          }
+        })
 
-    const handleLogin = () => {
+    const handleLogin = async() => {
         console.warn(email, password);
+        let result= await fetch("http://localhost:5000/login",{
+            method: 'post',
+            body: JSON.stringify({email,password}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        result = await result.json();
+        console.warn(result)
+        if(result.name){
+            localStorage.setItem("user",JSON.stringify(result));
+            navigate('/')
+
+        }else{
+            alert("Please enter correct details")
+        }
     };
 
     return (
