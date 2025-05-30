77422c02a32d2885502d6a3aaba1c846c1890727
Zyrus-47 <140943539+Zyrus-47@users.noreply.github.com>
2025-05-30 16:27:30 +0530

    made delete api

--- backend/index.js
+++ backend/index.js
@@ -49,5 +49,10 @@ app.get("/products",async(req,resp)=>{
     }
 })
 
+app.delete("/product/:id", async(req,resp)=>{
+    const result = await Product.deleteOne({_id:req.params.id})
+    resp.send(result);
+});
+
 
 app.listen(5000);
